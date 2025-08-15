ebf8d38ae9ff499f70b0f7227d37ae3a1a41480f
ilan990 <ilan2.assouline@gmail.com>
2025-08-15 20:03:48 +0200

    remove fr

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 py-4">
       <Section className="flex items-baseline">
-        <h1 className="text-lg font-bold text-primary">IlanAssouline.fr</h1>
+        <h1 className="text-lg font-bold text-primary">Ilan Assouline</h1>
         <div className="flex-1" />
         <ul className="flex items-center gap-2">
           <Link
